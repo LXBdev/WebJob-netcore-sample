8cc9766083eb188d97c4978c0148566e9c19e63e
Alex <xy.hochdorf@gmx.de>
2020-06-18 21:59:31 +0200

    inital commit

---
+++ Functions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Azure.WebJobs;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebJob_netcore_sample
+{
+
+        public class Functions
+        {
+            public async Task MyTimerTriggerOperation([TimerTrigger("0 * */5 * * *", RunOnStartup = true)] TimerInfo timerInfo, CancellationToken cancellationToken)
+            {
+                // Do some work...
+                await Task.Delay(100, cancellationToken);
+
+                Console.WriteLine("heavy work");
+            }
+        }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace WebJob_netcore_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var config = new ConfigurationBuilder()
+                //.AddEnvironmentVariables()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            foreach (var item in config.AsEnumerable())
+            {
+                Console.WriteLine(item);
+            }
+
+            HostBuilder builder = new HostBuilder();
+            builder.ConfigureWebJobs(b =>
+            {
+                b.AddAzureStorageCoreServices();
+                b.AddAzureStorage();
+                b.AddTimers();
+            });
+            IHost host = builder.Build();
+            using (host)
+            {
+                host.Run();
+            }
+        }
+    }
+}
