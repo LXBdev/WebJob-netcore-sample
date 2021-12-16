4960f9775b3c92acc60c67de60a9421324e1a522
Alexander Bender <alexander.bender@aitgmbh.de>
2021-12-16 10:55:19 +0100

    use net6.0

--- Functions.cs
+++ Functions.cs
@@ -4,31 +4,29 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace WebJob_netcore_sample
+namespace WebJob_netcore_sample;
+
+public class Functions
 {
+    private readonly IConfiguration _Configuration;
+    private readonly MyService _MyService;
 
-    public class Functions
+    public Functions(IConfiguration configuration, MyService myService)
     {
-        private readonly IConfiguration _Configuration;
-        private readonly MyService _MyService;
+        this._Configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        this._MyService = myService ?? throw new ArgumentNullException(nameof(myService));
+    }
 
-        public Functions(IConfiguration configuration, MyService myService)
+    public async Task MyTimerTriggerOperation([TimerTrigger("0/15 * * * * *", RunOnStartup = true)] TimerInfo timerInfo, CancellationToken cancellationToken)
+    {
+        // Do some work...
+        await Task.Delay(100, cancellationToken);
+
+        foreach (var item in _Configuration.AsEnumerable())
         {
-            this._Configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
-            this._MyService = myService ?? throw new ArgumentNullException(nameof(myService));
+            Console.WriteLine(item);
         }
 
-        public async Task MyTimerTriggerOperation([TimerTrigger("0/15 * * * * *", RunOnStartup = true)] TimerInfo timerInfo, CancellationToken cancellationToken)
-        {
-            // Do some work...
-            await Task.Delay(100, cancellationToken);
-
-            foreach (var item in _Configuration.AsEnumerable())
-            {
-                Console.WriteLine(item);
-            }
-
-            _MyService.Foo();
-        }
+        _MyService.Foo();
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,50 +2,41 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System.Threading.Tasks;
+using WebJob_netcore_sample;
 
-namespace WebJob_netcore_sample
+var builder = Host.CreateDefaultBuilder(args);
+builder.ConfigureWebJobs(b =>
 {
-    class Program
+    b.AddAzureStorageCoreServices();
+    b.AddAzureStorage();
+    b.AddTimers();
+});
+builder.ConfigureServices(services =>
+{
+    services.AddScoped<Functions>();
+    services.AddScoped<MyService>();
+});
+builder.ConfigureLogging((context, b) =>
+{
+    b.AddAzureWebAppDiagnostics();
+    b.AddApplicationInsights();
+    b.AddConsole();
+});
+builder.ConfigureAppConfiguration((context, configurationBuilder) =>
+{
+    configurationBuilder
+        .AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", optional: false)
+        .AddEnvironmentVariables();
+
+    if (context.HostingEnvironment.IsDevelopment())
     {
-        async static Task Main(string[] args)
-        {
-            var builder = Host.CreateDefaultBuilder(args);
-            builder.ConfigureWebJobs(b =>
-            {
-                b.AddAzureStorageCoreServices();
-                b.AddAzureStorage();
-                b.AddTimers();
-            });
-            builder.ConfigureServices(services =>
-            {
-                services.AddScoped<Functions>();
-                services.AddScoped<MyService>();
-            });
-            builder.ConfigureLogging((context, b) =>
-            {
-                b.AddAzureWebAppDiagnostics();
-                b.AddApplicationInsights();
-                b.AddConsole();
-            });
-            builder.ConfigureAppConfiguration((context, configurationBuilder) =>
-            {
-                configurationBuilder
-                    .AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", optional: false)
-                    .AddEnvironmentVariables();
-
-                if (context.HostingEnvironment.IsDevelopment())
-                {
-                    configurationBuilder
-                        .AddUserSecrets<Program>();
-                }
-            });
-
-            IHost host = builder.Build();
-            using (host)
-            {
-                await host.RunAsync();
-            }
-        }
+        configurationBuilder
+            .AddUserSecrets<Program>();
     }
+});
+
+IHost host = builder.Build();
+using (host)
+{
+    await host.RunAsync();
 }
